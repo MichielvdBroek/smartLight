12a9e54a873f3c2c63353dd663caf1150c0ce50c
Broek,Michiel M. van den <michiel.vandenbroek@student.fontys.nl>
2017-12-07 13:05:57 +0100

    database access
    ik kan in de firebase database die ik heb staan op console.firebase.google.com.
    Hier kan ik items toevoegen, en verwijderen. volgende stap is lezen van de data.

--- index.js
+++ index.js
@@ -2,8 +2,29 @@ const http = require('http');
 const hostname = '127.0.0.1';
 const port = 3000;
 
-//=> do on return from function.
+var Devs = [];
+Devs.push({
+    'name' : 'Michiel',
+    'age': 19
+});
+Devs.push({
+    'name' : 'Bas',
+    'age': 22
+});
+Devs.push({
+    'name' : 'Simon',
+    'age': 21
+});
+Devs.push({
+    'name' : 'Milan',
+    'age': 20
+});
 
+
+//=> do on return from function.
+//************************
+//*define server         *
+//************************
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
 //    res.setHeader('Content-type', 'text/plain');
@@ -15,23 +36,59 @@ const server = http.createServer((req, res) => {
     res.end();
 });
 
+//************************
+//*define firebase       *
+//************************
+var firebase = require('firebase');
+firebase.initializeApp({
+  apiKey: "AIzaSyAuLNhAnNdl6PFprTubZA_qS4is2moV9Uw",
+  serviceAccount: "./smartLight-b35ad744a03e.json",
+  databaseURL: "https://smartlight-188310.firebaseio.com",
+});
 
-function printHello(name)
+
+//************************
+//*define testfunction   *
+//************************
+/*function printHello(name)
 {
     if (name === "Michiel")
     {
         console.log("are you my god?");
     }
     console.log("hello " + name + ". How are you?\n");
-};
+};*/
 
 
+//printHello("Michiel");
+//printHello("Bas");
+console.log('\n\n');
 
-//main
 server.listen(port, hostname, () => {
     console.log(hostname + ": " + "server started on port " + port);
 });
 
-console.log('\n\n');
-printHello("Michiel");
-printHello("Bas");
+var firebaseRef = firebase.database().ref();
+var devNamesRef = firebaseRef.child('devs');
+
+
+//************************
+//*clear 'devs' in db    *
+//************************
+firebaseRef.update({
+    'devs': null
+});
+
+//************************
+//*push devs to in db    *
+//************************
+for (var i = 0; i < 4; i++) 
+{
+    var devNamesEntry = devNamesRef.push();
+    
+    console.log("a push key for the database has been created: " + devNamesEntry.key);
+    devNamesEntry.set({
+        name: Devs[i].name,
+        age: Devs[i].age
+    });
+}
